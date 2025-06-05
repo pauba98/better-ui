8c65f3a27aa603dfb20cf5b51d5dde3b21327644
Pau Blas <pblas@bettercare.es>
2025-06-05 11:29:38 +0200

    checkbox test

--- src/components/Checkbox/Checkbox.test.tsx
+++ src/components/Checkbox/Checkbox.test.tsx
@@ -1,18 +1,39 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-
+import { getByRole, render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
 import Checkbox from "./Checkbox";
 
 describe("Checkbox", () => {
     test("renders the Checkbox component", () => {
         render(<Checkbox>Hello World</Checkbox>);
-        const button = screen.getByRole('button');
-        expect(button).toHaveTextContent('Hello World')
+        expect(screen.getByRole('button')).toHaveTextContent('Hello World')
+        expect(screen.getByRole('checkbox')).not.toBeChecked()
     });
 
-    test("renders the Checkbox component", () => {
+    test("click the checkbox", async () => {
         render(<Checkbox>
             Hello World
         </Checkbox>);
+        await userEvent.click(screen.getByRole('checkbox'));
+        expect(screen.getByRole('checkbox')).toBeChecked()
     });
+
+    test("click the checkbox button", async () => {
+        render(<Checkbox>
+            Hello World
+        </Checkbox>);
+        await userEvent.click(screen.getByRole('button'));
+        expect(screen.getByRole('checkbox')).toBeChecked()
+    });
+    
+
+    test("click the checkbox button with default checked", async () => {
+        render(<Checkbox defaultChecked={true}>
+            Hello World
+        </Checkbox>);
+        await userEvent.click(screen.getByRole('button'));
+        expect(screen.getByRole('checkbox')).not.toBeChecked()
+    });
+
 });
